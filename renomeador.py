e59aa5f437904c5f69ee02a34625e928472f5824
METALZILO <adrianosilvaoliveiradejesus@gmail.com>
2019-05-07 13:30:45 -0300

    versão 0.1

--- renomeador.py
+++ renomeador.py
@@ -1,19 +1,46 @@
-# importing os module 
-import os 
-  
-path = ''
+import os,glob
+from pathlib import Path
 
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.txt' in file:
-            files.append(os.path.join(r, file))
+mypath = Path().absolute()
 
-for f in files:
-    print(f)
+#Lista de códigos de referência dos óculos
+lista_codigo_oculos = []
 
-os.rename('a.txt', 'b.kml')
+#quantidade de óculos para ser catálogada
+quantidade_de_oculos = int(input("Quantidade de óculos para catalogar: "))
+
+#Quantidade de óculos já catalogado
+contador = 0
+
+while contador < quantidade_de_oculos:
+    #Código de referencia do óculos
+    código_referência = str(input("Código de Referência: "))
+    #Adicionando código a lista da códigos
+    lista_codigo_oculos.append(código_referência.upper())
+    contador += 1
+
+os.chdir(mypath)
+
+#numero do item
+item=1
+
+#index do item
+itemIndex = 0
+
+for (index,file) in enumerate(glob.glob('.JPG')):
+    src=file
+    novoNome=str(index + 1)+" "+lista_codigo_oculos[itemIndex]+".jpg"
+    itemIndex+=1
+    item += 1
+    os.rename(src,novoNome)
+    if(item > quantidade_de_oculos):
+        itemIndex = 0
+        item = 0
+        
+
+
+
+
 
 #links de apoio 
 #https://www.mkyong.com/python/python-how-to-list-all-files-in-a-directory/
